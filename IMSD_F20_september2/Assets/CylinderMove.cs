4332a80c64c4dfe4be1c7b4b6c546f84add29c8a
Mykhal Parson <mykhal.reign@mail.citytech.cuny.edu>
2020-09-09 02:00:23 -0400

    Added start of assignment to repos folder

---
+++ IMSD_F20_september2/Assets/CylinderMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderMove : MonoBehaviour
+{
+    public Transform myTransform;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+          myTransform.position = new Vector3 (1,2,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position = myTransform.position + new Vector3(5,3,0) * speed * Time.deltaTime;
+    }
+}
